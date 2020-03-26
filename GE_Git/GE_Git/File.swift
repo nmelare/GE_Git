a7eb63d7d6ff247f792816500c759cca408092d7
Nathalia <nathaliamelare@gmail.com>
2020-03-26 18:09:59 -0300

    Add outro file direto da dev

---
+++ GE_Git/GE_Git/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  GE_Git
+//
+//  Created by Nathalia Melare on 26/03/20.
+//  Copyright © 2020 Nathalia Melare. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+func teste () {
+    var teste = String()
+}
